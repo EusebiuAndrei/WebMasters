1d9177909a33d215b956c96acbf64211d77a4602
limelier <tudor.iacobescu.1@gmail.com>
2020-05-07 15:52:52 +0300

    service done

--- api/routes/accidentsRouter.js
+++ api/routes/accidentsRouter.js
@@ -1,8 +1,20 @@
 const httpStatus = require('http-status-codes');
 const router = require('../../Router');
+const { accidentService } = require('../../services');
 
 const ROUTE_BASE = '/api/accidents';
 
 router.get(`${ROUTE_BASE}`, (req, res) => {
 	res.status(httpStatus.OK).json({ success: true, msg: 'Hello' });
+});
+
+router.get(`${ROUTE_BASE}/chart_data`, async (req, res) => {
+	const result = await accidentService.getBuckets({
+		bucketType: 'column',
+		bucketColumn: 'state',
+		valueType: 'count',
+		filters: [],
+	});
+
+	res.status(httpStatus.OK).json(result);
 });
--- services/AccidentService.js
+++ services/AccidentService.js
@@ -1,9 +1,65 @@
 const Logger = require('../loaders/logger');
 
+function groupBuilder({ bucketType, bucketColumn, timeChart, valueType }) {
+	let _id;
+	if (bucketType === 'column') {
+		_id = `$${bucketColumn}`;
+	} else { // query.bucketType === 'time'
+		const dateColumn = `$${timeChart.timeAxisBasedOn}Time`
+		_id = {
+			year: {$year: dateColumn}
+		};
+		if (timeChart.bucketSize === 'week') {
+			_id.week = {$week: dateColumn};
+		} else {
+			_id.month = {$month: dateColumn};
+			if (timeChart.bucketSize === 'day') {
+				_id.month = {$dayOfMonth: dateColumn};
+			}
+		}
+	}
+
+	let value;
+	if (valueType === 'count') {
+		value = {$sum: 1};
+	} else {
+		value = {
+			[`$${valueType.type}`]: `$${valueType.column}`
+		};
+	}
+
+	return { _id, value };
+}
+
+function filterIntoMatch({ column, constraint, value }) {
+	return {
+		[column]: {
+			[`$${constraint}`]: value,
+		},
+	};
+}
+
 class AccidentService {
 	constructor({ db, services }) {
 		this.db = db;
 		this.services = services;
+	}
+
+	async getBuckets(query) {
+		let aggregate = this.db.accidents.aggregate();
+
+		for (const filter of query.filters) {
+			aggregate = aggregate.match(filterIntoMatch(filter))
+		}
+
+		aggregate = aggregate.group(groupBuilder(query));
+
+		const result = await aggregate.exec();
+
+		const bucketLabels = result.map(item => item._id);
+		const values = result.map(item => item.value);
+
+		return { bucketLabels, values };
 	}
 }
 
