7d50f7882a6cc132543ab79b26ba21115769d67c
Daniel Bicu <daniel.bicu008@yahoo.com>
2020-06-07 23:40:08 +0300

    readded exports

--- frontend/public/scripts/components/Export.js
+++ frontend/public/scripts/components/Export.js
@@ -1,22 +1,82 @@
+import StateManager from '../utils/StateManager.js';
+
 const Export = () => {
 	return `
             <div class="export-container">
                 <label>Export as
-                    <select style="display: block;">
+                    <select id="exportType" style="display: block;">
                         <option value="pdf">PDF</option>
                         <option value="png">PNG</option>
-                        <option value="csv">CSV</option>
+						<option value="csv">CSV</option>
+						<option value="svg">SVG</option>
                     </select>
                 </label>
 
-                <p>Download</p>
+                <a id="downloadBtn">Download </a>
 
                 <p>Send Email</p>
-            </div>
+            </div> 
         `;
 };
 
-const addEventsListeners = () => {};
+const addEventsListeners = () => {
+	const exportList = document.getElementById('exportType');
+	const dwnButton = document.getElementById('downloadBtn');
+
+	if (dwnButton) {
+		dwnButton.addEventListener('click', (event) => {
+			var selectedValue = exportList.options[exportList.selectedIndex].value;
+			if (selectedValue == 'csv') {
+				alert('csv!');
+
+				console.log(StateManager.getStateForVisual().fetchedData);
+			} else if (selectedValue == 'pdf') {
+				var pie = false;
+				var describeTxt = document.getElementsByClassName(
+					'bar_graph__header__title',
+				)[0];
+
+				if (describeTxt) describeTxt = describeTxt.textContent;
+
+				console.log(describeTxt);
+				var toExport = document.querySelector('#chart');
+				if (toExport == null) {
+					toExport = document.querySelector('#pieChart');
+					pie = true;
+				}
+				var canvasImage = toExport.toDataURL('image/png', 1.0);
+
+				var exportedDoc = new jsPDF('landscape');
+				exportedDoc.setFontSize(10);
+
+				exportedDoc.text(15, 12, describeTxt ? describeTxt : '');
+
+				if (pie) exportedDoc.addImage(canvasImage, 'PNG', 10, 10, 200, 150);
+				else exportedDoc.addImage(canvasImage, 'PNG', 10, 10, 280, 150);
+
+				exportedDoc.save('chart.pdf');
+			} else if (selectedValue == 'png') {
+				alert('png!');
+
+				var toExport = document.querySelector('#chart');
+				if (toExport == null) {
+					toExport = document.querySelector('#pieChart');
+					pie = true;
+				}
+
+				const elem = document.createElement('a');
+
+				document.body.appendChild(elem);
+
+				elem.href = toExport.toDataURL('image/png', 1.0);
+				elem.download = 'chart.png';
+				elem.click();
+
+				document.body.removeChild(elem);
+			}
+		});
+	}
+};
 
 export default Export;
 export { addEventsListeners };
