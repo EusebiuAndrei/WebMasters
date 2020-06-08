34630074dbb1508eefff9fb1e422032159fb51b0
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2020-06-08 12:44:35 +0300

    Remove useless graph components

--- frontend/public/scripts/components/BarGraph.js
+++ frontend/public/scripts/components/BarGraph.js
@@ -1,11 +1,6 @@
 const BarGraph = () => {
 	return `
         <div class="container__bar bar_graph">
-            <div class="bar_graph__header">
-                <span class="bar_graph__header__title">Number of accidents over time</span>
-
-                <button class="bar_graph__header__settings_button button button--settings"></button>
-            </div>
             <div class="bar_graph__content" id="chart_container">
                 <canvas id="chart"></canvas>
             </div>
